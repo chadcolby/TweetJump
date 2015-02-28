cc3b71123dde81902d79ccabdfbc896ab6975405
Chad Colby <chad@gestotech.com>
2015-02-27 17:51:29 -0800

    logged

--- Tweet Dodge/bmwAppDelegate.m
+++ Tweet Dodge/bmwAppDelegate.m
@@ -15,12 +15,6 @@
 
 /*==== theis ====*/
     
-    NSLog(@"this is a test");
-
-    // Override point for customization after application launch.
-    // this doesn't matter at all
-    // time is a cruel bitch
-    
     NSLog(@"i wonder what i meant by that");
 
     return YES;
